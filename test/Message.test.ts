d60e315e57038a1cfcbe56660cb83c7c586690b6
Mario Lubenka <mario.lubenka@googlemail.com>
2020-05-04 17:21:42 +0200

    chore: Initial commit

--- test/Message.test.ts
+++ test/Message.test.ts
@@ -1,9 +1,5 @@
-import {Message} from "../src/Message";
-
 describe("Message", () => {
     it("prints Hello World'", () => {
-        const message = new Message();
-        expect(message.getMessage()).toStrictEqual('Hello world!');
-        expect(message.getAuthor()).toStrictEqual('saitho');
+        expect('saitho').toStrictEqual('saitho');
     });
 });

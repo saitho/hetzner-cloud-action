843af0f5f5b7bb420c2480acb8bb7c5c8f7f9151
Mario Lubenka <mario.lubenka@googlemail.com>
2020-05-10 15:06:19 +0200

    fix: allow removing server when post-create actions failed

--- src/worker/worker.ts
+++ src/worker/worker.ts
@@ -1,15 +1,18 @@
 import * as hcloud from 'hcloud-js';
-import {Server} from "../interfaces/server";
+
+export type TOutputFunc = (name: string, value: any) => void
 
 export abstract class ServerWorker<T> {
     protected client: hcloud.Client = null;
     protected data: T = null;
+    protected setOutput: TOutputFunc = null;
 
-    constructor(client: hcloud.Client) {
+    constructor(client: hcloud.Client, outputFunc: TOutputFunc) {
         this.client = client;
+        this.setOutput = outputFunc;
     }
 
-    public abstract process(): Promise<Server>;
+    public abstract process(): Promise<void>;
 
     protected waitUntilFinished(actionId: string) {
         return new Promise<string>(async (resolve) => {
